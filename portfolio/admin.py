9be929decd2f35ccfb15b25152672f661605de7e
josue130 <josuelopez67i90@gmail.com>
2023-09-10 23:13:54 -0600

    first commit

---
+++ portfolio/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import MyProjects
+
+admin.site.register(MyProjects)
+# Register your models here.
---
+++ portfolio/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import get_object_or_404, render
+from .models import MyProjects
+# Create your views here.
+def home(request):
+    projects = MyProjects.objects.all() # Trae todos lo proyectos dentro de la DB
+    return render(request, "home.html", {"projects": projects})
+
+def proyect_details(request, proyect_id):
+    proyect = get_object_or_404(MyProjects, pk=proyect_id)
+    return render(request, "proyect_details.html", {"single_proyect": proyect})
