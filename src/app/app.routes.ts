936577a342a02c3dd3131f9f4ababc1c10f4eb8d
josue130 <josuelopez67i90@gmail.com>
2024-10-16 13:44:43 -0600

    Add project page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,16 @@ export const routes: Routes = [
         path : '',
         children: [
             {
-                path:'',
+                path:'home',
                 loadComponent: () => import('./home/home.component').then(m => m.HomeComponent),
+            },
+            {
+                path:'projects',
+                loadComponent:() => import('./projects/projects.component').then(m=>m.ProjectsComponent)
+            },
+            {
+                path:'**',
+                redirectTo:'home'
             }
         ]
     }
