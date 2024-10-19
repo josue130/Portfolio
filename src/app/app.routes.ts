5605b4f0fcf728715391b211837e50f45adbc672
josue130 <josuelopez67i90@gmail.com>
2024-10-18 23:31:34 -0600

    Refactor: remove project page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,14 +9,6 @@ export const routes: Routes = [
                 loadComponent: () => import('./home/home.component').then(m => m.HomeComponent),
             },
             {
-                path:'projects',
-                loadComponent:() => import('./projects/projects.component').then(m=>m.ProjectsComponent)
-            },
-            {
-                path:'skills',
-                loadComponent:() => import('./skills/skills.component').then(m=>m.SkillsComponent)
-            },
-            {
                 path:'**',
                 redirectTo:'home'
             }
