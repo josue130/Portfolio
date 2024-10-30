6d8c42e2a260c7f3fcc074ef7f37866294dd0550
josue130 <josuelopez67i90@gmail.com>
2024-10-29 20:27:14 -0600

    feat(prjoects): add projects information

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,5 +12,29 @@ import { AboutMeComponent } from "./ui/about-me/about-me.component";
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  projects = [
+    {
+      title: 'Workout Tracker',
+      description: 'This project involves creating a backend system for a workout tracker application with clean architecture.',
+      imageUrl: 'https://assets.clevelandclinic.org/transform/71749feb-e22a-468c-8538-5d4f1b70d2a6/seven-minute-workout-exercises-1310070967-770x533-1_jpg',
+      technologies: ["C#", ".NET","ASP.NET","SQL Server"],
+      codeSource:"https://github.com/josue130/WorkoutTracker"
+    },
+    {
+      title: 'Shoe Store',
+      description: 'This project demonstrates a simple microservices architecture using three APIs',
+      imageUrl: 'assets/images/ShoeStoreDiagram.webp',
+      technologies: ["C#", ".NET","ASP.NET","SQL Server"],
+      codeSource:"https://github.com/josue130/ShoeStore"
+    }
+    ,
+    {
+      title: 'Portfolio',
+      description: 'On this website, you will find information about my experience, the projects I have worked on, and some details about me.',
+      imageUrl: 'assets/images/MyPortfolio.webp',
+      technologies: ["Angular","Boostrap","PRIMENG"],
+      codeSource:"https://github.com/josue130/Portfolio"
+    }
+  ];
 
 }
--- src/app/home/ui/projects/projects.component.ts
+++ src/app/home/ui/projects/projects.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TagModule } from 'primeng/tag';
+interface Project {
+  title: string;
+  description: string;
+  imageUrl: string;
+  technologies: string[];
+  codeSource:string;
+}
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,TagModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
-  
+  @Input() projects: Project[] = [];
 }
