eca8d110582b88435fb4c8d4e8779a779dc976e9
josue130 <josuelopez67i90@gmail.com>
2025-01-31 16:40:58 -0600

    docs: update home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -29,18 +29,19 @@ export class HomeComponent {
   ngOnInit() {
     this.projects = [
       {
+        title: 'Spending Tracker',
+        description: 'A financial tracking tool that allows users to record their expenses and incomes by categories, and get monthly balances.',
+        technologies: ["C#", ".NET", "xUnit", "Fluent Assertions", "Moq","ASP.NET", "SQL Server","Redis",
+          "FreecurrencyAPI"],
+        codeSource: "https://github.com/josue130/SpendingTracker"
+      },
+      {
         title: 'Game Rating',
         description: 'GameRating is designed to manage and rate video games. Developed using technologies such as .NET 8, C#, ASP.NET Core and Minimal APIs',
         technologies: ["C#", ".NET", "ASP.NET", "SQL Server","GraphQL","Azure"],
         codeSource: "https://github.com/josue130/GameRating"
       },
-      {
-        title: 'Spending Tracker',
-        description: 'A financial tracking tool that allows users to record their expenses and incomes by categories, and get monthly balances.',
-        technologies: ["C#", ".NET", "ASP.NET", "SQL Server","Redis",
-          "FreecurrencyAPI"],
-        codeSource: "https://github.com/josue130/SpendingTracker"
-      },
+      
       {
         title: 'Shoe Store',
         description: 'This project demonstrates a simple microservices architecture using three APIs',
@@ -50,7 +51,7 @@ export class HomeComponent {
       {
         title: 'Workout Tracker',
         description: 'This project involves creating a backend system for a workout tracker application.',
-        technologies: ["C#", ".NET", "ASP.NET", "SQL Server"],
+        technologies: ["C#", ".NET", "ASP.NET", "SQL Server","xUnit", "Fluent Assertions", "Moq"],
         codeSource: "https://github.com/josue130/WorkoutTracker"
       },
       {
